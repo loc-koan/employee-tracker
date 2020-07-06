aca62aace0dfd6e35049448ceae749f95d0016d2
loc-koan <loc.koan@yahoo.com>
2020-07-05 20:42:46 -0500

    added seed file

---
+++ seed.sql
@@ -0,0 +1,27 @@
+/* Seeds for SQL table */
+USE reynholm_industries_db;
+
+/* department */ 
+INSERT INTO department (dept_name)
+VALUES 
+    ("Information Technology"), 
+    ("Research and Development"), 
+    ("Engineering");
+
+/* role */ 
+INSERT INTO role (title, salary, dept_id)
+VALUES 
+    ("Manager",50000,1), 
+    ("Tech",40000,1), 
+    ("Instructor",100000,2), 
+    ("Developer",90000,2);
+
+/* employee */ 
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+    ("Jen","Barber",1,1), 
+    ("Maurice","Moss",2,1), 
+    ("Roy","Trenneman",2,1), 
+    ("Tish","McNeel",3,3), 
+    ("Brandon","Gatlin",4,3), 
+    ("Peter","Winston",4,3);
